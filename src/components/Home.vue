d55c2bad2cbcac4d9f8a2c4c09f18e3f05257b32
simon <674949287@qq.com>
2020-04-07 22:45:37 +0800

    Prerendering

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+
+    <div>我是首页
+  <router-link to="/test">去测试页面</router-link>
+  <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+
+<style scoped>
+
+</style>
